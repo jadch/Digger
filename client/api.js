69a65af8353cf1340735d2b1667cb8b8fd00489f
Jad ch <jad.chalita@gmail.com>
2017-11-19 19:38:13 +0100

    Now sending discogs requests from the client-side

--- client/api.js
+++ client/api.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 
-const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000/api'
+const discogs = axios.create({
+  baseURL: 'https://api.discogs.com/'
 })
 
+// Function that gets a random Discogs master release
 export function getRandomDiscogsRelease () {
-  return api.get('/random')
+  // PS: Dicogs release IDs seem to be running from 113 to 1'268'960
+  let random = Math.random() * (1268960 - 113) + 113
+  let randomString = Math.floor(random).toString()
+  return discogs.get('masters/' + randomString)
   .then(response => response.data)
   .catch(error => {
     console.error('Error getting a random release, ', error)
