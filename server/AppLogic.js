373140dde4b246de8c476e5fae9e438a866d68cc
Jad ch <jad.chalita@gmail.com>
2017-11-25 10:54:29 +0100

    Now using eslint Airbnb in /server

--- server/AppLogic.js
+++ server/AppLogic.js
@@ -1,26 +1,26 @@
-const axios = require('axios')
+const axios = require('axios');
 
 const discogs = axios.create({
-  baseURL: 'https://api.discogs.com/'
-})
+  baseURL: 'https://api.discogs.com/',
+});
 
 // Function that gets a Discogs master release, given Discogs master ID
-export function getDiscogsRelease (id) {
-  return discogs.get('masters/' + id)
-  .then(response => response.data)
-  .catch(error => {
-    console.error('Error getting master release, ', error)
-  })
+export function getDiscogsRelease(id) {
+  return discogs.get(`masters/${id}`)
+    .then(response => response.data)
+    .catch((error) => {
+      console.error('Error getting master release, ', error);
+    });
 }
 
 // Function that gets a random Discogs master release
-export function getRandomDiscogsRelease () {
+export function getRandomDiscogsRelease() {
   // PS: Dicogs release IDs seem to be running from 113 to 1'268'960
-  let random = Math.random() * (1268960 - 113) + 113
-  let randomString = Math.floor(random).toString()
-  return discogs.get('masters/' + randomString)
-  .then(response => response.data)
-  .catch(error => {
-    console.error('Error getting a random release, ', error)
-  })
+  const random = Math.random() * (1268960 - 113) + 113;
+  const randomString = Math.floor(random).toString();
+  return discogs.get(`masters/${randomString}`)
+    .then(response => response.data)
+    .catch((error) => {
+      console.error('Error getting a random release, ', error);
+    });
 }
--- server/app.js
+++ server/app.js
@@ -1,46 +1,44 @@
-require('import-export')
-require('./AppLogic.js')
+require('import-export');
+require('./AppLogic.js');
 
-const express = require('express')
-const logger = require('morgan')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const index = require('./routes/index')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const index = require('./routes/index');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 if (app.get('env') === 'development') {
-  app.use(
-    cors({
-      origin: 'http://localhost:8080'
-    })
-  )
+  app.use(cors({
+    origin: 'http://localhost:8080',
+  }));
 }
 
-app.use('/api', index)
+app.use('/api', index);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-module.exports = app
+module.exports = app;
