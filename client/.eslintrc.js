62d4de16cb50e744841f5f024fdc313f67f0bf00
Jad ch <jad.chalita@gmail.com>
2017-11-27 14:08:26 +0100

    trying to configure eslint to work with .vue files

---
+++ client/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["airbnb", "plugin:vue/essential"],
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    mocha: true
+  }
+}
