7927b10eef89d2fac2bee3495c7355f4e2b69006
Jad ch <jad.chalita@gmail.com>
2017-12-24 16:23:04 +0100

    refactor: created functions.js

--- digger-serverless/handler.js
+++ digger-serverless/handler.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 const sql = require('mssql');
 const AWS = require('aws-sdk');
 
+const { queryElectronicTableLength, queryAlbumAtRow } = require('./functions');
+
 dotenv.load();
 
 // Creating connection to the database
@@ -17,24 +19,6 @@ const config = {
   },
 };
 
-// Function that returns the Electronic table length (the number of rows in the table)
-function queryElectronicTableLength(pool) {
-  return pool.request()
-    .query(`SELECT COUNT(MasterId)
-    FROM Electronic`)
-    .then(result => parseInt(result.recordsets[0][0][''], 10));
-}
-
-// Function that gets an album from the Electronic table,
-// at a specified row position
-function queryAlbumAtRow(pool, row) {
-  return pool.request()
-    .input('row', sql.Int, row)
-    .query(` SELECT * FROM Electronic
-     ORDER BY MasterId
-     OFFSET @row ROWS FETCH NEXT 1 ROWS ONLY`)
-    .then(result => result.recordsets[0][0]);
-}
 
 module.exports.fetchElectronicTableLength = (event, context, callback) => {
     new sql.ConnectionPool(config).connect().then(pool => {
@@ -54,7 +38,6 @@ module.exports.fetchElectronicTableLength = (event, context, callback) => {
 };
 
 module.exports.fetchRandomMasterID = (event, context, callback) => {
-  // const table = 'Electronic';
   //
   // new sql.ConnectionPool(config).connect().then(pool => {
   //   queryElectronicTableLength(pool).then(maxLen => {
